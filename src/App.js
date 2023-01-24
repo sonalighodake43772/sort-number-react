ce593aaa59558c01b4d195ce7d37f8d8f5c406c5
sonalighodake43772 <sonalighodake867>
2023-01-24 18:59:14 +0530

    ascending and decending order

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useMemo } from "react";
+import Demo from "./component/Demo/Demo";
+import Button from "./component/UI/Button";
 
 function App() {
+  const [listTitle, setListTitle] = useState("My List");
+  const [descending, setDesending] = useState([]);
+  const [btnText, setBtnText] = useState("Descending order");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const changeOrder = () => {
+    let text = btnText;
+
+    if (text === "Descending order")
+       {
+         setBtnText("Ascending Order");
+         const descendingSort = listItems.sort((a, b) => b - a);
+         setDesending(descendingSort);
+       }
+    else
+       {
+         setBtnText("Descending order");
+         const assendingSort = listItems.sort((a, b) => a - b);
+         setDesending(assendingSort);
+       }
+  };
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="app">
+      <Demo title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>change list Title</Button>
+      <Button onClick={changeOrder}>{btnText}</Button>
     </div>
   );
 }
-
 export default App;
